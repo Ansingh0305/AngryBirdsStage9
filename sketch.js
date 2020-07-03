beebe51a1f725acd63a8fee531e7a468f2ae4091
Ansingh0305 <59765993+Ansingh0305@users.noreply.github.com>
2020-07-03 16:47:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ var bg = "sprites/bg1.png";
 var score = 0;
 
 function preload() {
-    getBackgroundImg();
+    backgroundImg=loadImage("sprites/bg.png")
 }
 
 function setup(){
@@ -101,7 +101,7 @@ function keyPressed(){
     }
 }
 
-async function getBackgroundImg(){
+/*async function getBackgroundImg(){
     var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
 
@@ -117,4 +117,4 @@ async function getBackgroundImg(){
 
     backgroundImg = loadImage(bg);
     console.log(backgroundImg);
-}
+}*/
